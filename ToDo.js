54d031a20e7c22b4c71aa91940707a2ff523be6e
Burhanuddin910 <burhanuddin.work910@gmail.com>
2025-05-04 01:34:13 +0500

    Add files via upload

---
+++ ToDo.js
@@ -0,0 +1,50 @@
+let ToDoList_TASK = [];
+let ToDoList_DATE = [];
+RenderTasks();
+
+    const add_button = document.querySelector('.add-button');
+    add_button.addEventListener('click',ADD);
+
+function ADD()
+{
+    let task = document.querySelector('.task-input').value;
+    let date = document.querySelector('.date-input').value;
+
+    if((task === '') || (date === ''))
+    {
+        alert('Please enter both task and date');
+        return;
+    }
+
+    ToDoList_TASK.push(task);
+    ToDoList_DATE.push(date);
+
+    document.querySelector('.task-input').value = '';
+    document.querySelector('.date-input').value = '';
+
+    RenderTasks();
+}
+
+function RenderTasks()
+{
+    let TaskHtml = '';
+    for(let i = 0; i < ToDoList_TASK.length; i++)
+    {
+        TaskHtml = TaskHtml + `<span class="display-task">${ToDoList_TASK[i]}</span>
+                               <span class="display-date">${ToDoList_DATE[i]}</span>
+                               <button class="delete-button" onclick="DeleteTask(${i})">Delete</button>`;
+    }
+    document.querySelector('.task-container').innerHTML = TaskHtml;
+}
+function DeleteTask(index)
+{
+    ToDoList_DATE.splice(index,1);
+    ToDoList_TASK.splice(index,1);
+    RenderTasks();
+}
+
+flatpickr("#datepicker", {
+    dateFormat: "Y-m-d", // Customize format as needed
+    altInput: true,
+    altFormat: "F j, Y"
+  });
